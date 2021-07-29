d4d55faf82ab22078a790fd0728f3f63afdf8d29
José David García Rodríguez <josdagaro@gmail.com>
2021-07-28 21:15:40 -0500

    Prepare login for SOI

--- soi.js
+++ soi.js
@@ -11,8 +11,67 @@ async function run(person, params) {
 
 async function main(page, args) {
   console.log('[INFO]: Running application SOI...');
-  page = await login(page, config.providers.soi);
-  page = await chooseContributor(page, args);
-  page = await search(page, args);
-  page = await downloadPdfs(page, config.defaults.downloadsPath);
+  page = await login(page, config.providers.soi, args);
+  // page = await chooseContributor(page, args);
+  // page = await search(page, args);
+  // page = await downloadPdfs(page, config.defaults.downloadsPath);
 }
+
+async function login(page, soiConfig, args) {
+  await page.goto(soiConfig.urls.login, { timeout: 60000 });
+  await page.waitForSelector('input[name=numeroIdUsuario');
+  await page.focus('input[name=numeroIdUsuario');
+
+  // Fill User NIT
+  await page.keyboard.type(soiConfig.user.nit);
+  await page.select('#tipoIdUsuario', extractNitTypeValue(soiConfig.user.nitType));
+
+  // Fill Company NIT
+  await page.focus('input[name=numeroIdEmpresa');
+  await page.keyboard.type(args.params.contributor);
+  await page.select('#tipoIdEmpresa', "2");
+
+  // Fill password
+  await page.focus('input[name=claveUsuario');
+  await page.keyboard.type(soiConfig.user.pass);
+
+
+  // Login
+  // await page.click('#login');
+  // await page.waitForNavigation();
+  await new Promise(resolve => setTimeout(resolve, 500)).catch();
+  await page.screenshot({ path: config.defaults.screenShotsPath + '/soi/login.png' });
+  return page;
+}
+
+function extractNitTypeValue(nitType) {
+  let value = "1";
+
+  switch (nitType) {
+    case 'CC':
+      value = "1";
+      break;
+    case 'CD':
+      value = "8";
+      break;
+    case 'CE':
+      value = "6";
+      break;
+    case 'PA':
+      value = "5";
+      break;
+    case 'PE':
+      value = "9";
+      break;
+    case 'RC':
+      value = "4";
+      break;
+    case 'TI':
+      value = "3";
+      break;
+  }
+
+  return value;
+}
+
+exports.run = run;
